c7cd92b038869e801269adcf550963d20c8913d6
Irina COVALI <icovali@pentalog.com>
2018-02-14 12:09:05 +0200

    no movie poster fix

--- src/app/components/found-movie/found-movie.component.ts
+++ src/app/components/found-movie/found-movie.component.ts
@@ -4,18 +4,21 @@ import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'found-movie-item',
-  templateUrl: './found-movie.component.html'
+  templateUrl: './found-movie.component.html',
+  styleUrls: ['./found-movie.component.scss']
 })
 export class FoundMovieComponent implements OnInit {
   @Input() movie: Movie;
 
-  image: string;
-  private moviePath = 'https://image.tmdb.org/t/p/w92';
+  imageSrc: string;
+  poster: boolean;
+  private moviePath: string = 'https://image.tmdb.org/t/p/w92';
 
   constructor() { }
 
   ngOnInit() {
-    this.image = this.movie.poster_path ? this.moviePath + this.movie.poster_path : 'http://www.theemailcompany.com/wp-content/uploads/2016/02/no-image-placeholder-big-300x200.jpg';
+    this.poster = this.movie.poster_path ? true : false;
+    this.imageSrc = this.moviePath + this.movie.poster_path;
   }
 
 }
